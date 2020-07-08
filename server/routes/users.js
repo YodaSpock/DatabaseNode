513dd9508a7633bf419ad8eeb2b4467fa0b6810e
Isaac Spanier <ispanier@iastate.edu>
2020-07-08 11:37:25 -0500

    changed App.js file

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var dbConnection = require('../db/index');
 const e = require('express');
 
-router.get('/', function(req, res, next){
+router.get('/users', function(req, res, next){
     dbConnection.query('SELECT * FROM users', function(err,rows){
         if(err){
             req.flash('error', err);
