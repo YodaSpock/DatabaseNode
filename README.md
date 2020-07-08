eda9f945beede08b23e25a443c0c50af327ef622
Isaac Spanier <ispanier@iastate.edu>
2020-07-07 19:12:00 -0500

    shit broke

--- README.md
+++ README.md
@@ -2,15 +2,21 @@
 
 Steps Taken to Setup 
 
+## Installs
+```
 npm install express
 npm install mysql
 npm install -D nodemon
+```
 
+```
 npm install -D -g concurrently
-
+```
+## Folders 
 Created Server Folder 
 
 Created SubFolders db, routes with index.js files
+
 Created a server.js file 
 
 Created Client React Folder
--- server/db/index.js
+++ server/db/index.js
@@ -1,40 +1,79 @@
 const mysql = require('mysql');
+const express = require('express');
 
-const pool =  mysql.createPool({
-    connectionLimit: 10,
-    password: 'Chewbacca980',
+const mysqlConnection =  mysql.createConnection({
     user: 'root',
+    password: 'Chewbacca980',
     database: 'database_node',
     host: 'localhost',
     port: '3306',
+    multipleStatements: true,
 
 });
 
-let users = {};
+mysqlConnection.connect((err) => {
+    if(!err){
+        console.log("DB Connection Successful")
+    }
+    else{
+        console.log('DB Conncection Failed \n Error: ' + JSON.stringify(err,undefined,2));
+    }
+})
 
-users.all = () => {
+app.get('/users', (req, res) => {
+    mysqlConnection.query('SELECT * FROM users', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
 
-    return new Promise((resolve, reject) => {
-        pool.query(`SELECT * FROM users`, (err, results) => {
-            if(err){
-                return reject(err);
-            }
-            return resolve(results);
-        })
-    });
-};
+//Get a user
+app.get('/users/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM users WHERE userid = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
 
-users.one = (id) => {
+//Delete a user
+app.delete('/users/:id', (req, res) => {
+    mysqlConnection.query('DELETE FROM Employee WHERE userid = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Deleted successfully.');
+        else
+            console.log(err);
+    })
+});
 
-    return new Promise((resolve, reject) => {
-        pool.query(`SELECT * FROM users WHERE id = ?`, [id], (err, results) => {
-            if(err){
-                return reject(err);
-            }
-            return resolve(results);
-        })
-    });
-};
+//Add a user
+app.post('/users', (req, res) => {
+    let emp = req.body;
+    var sql = "SET @userid = ?;SET @username = ?;SET @password = ?; \
+    CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
+    mysqlConnection.query(sql, [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary], (err, rows, fields) => {
+        if (!err)
+            rows.forEach(element => {
+                if(element.constructor == Array)
+                res.send('Inserted employee id : '+element[0].EmpID);
+            });
+        else
+            console.log(err);
+    })
+});
 
-
-module.exports = users;
+//Update a user
+app.put('/users', (req, res) => {
+    let user = req.body;
+    var sql = "SET @userid = ?;SET @username = ?;SET @password = ?;\
+    CALL EmployeeAddOrEdit(@userid,@username,@password);";
+    mysqlConnection.query(sql, [user.userid, user.username, user.password], (err, rows, fields) => {
+        if (!err)
+            res.send('Updated successfully');
+        else
+            console.log(err);
+    })
+});
